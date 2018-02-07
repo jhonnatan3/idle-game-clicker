f5c28c6d66ee0c5a0f35b070f3faaa66c1901c60
Artur M <artur@butio.pl>
2018-02-07 18:46:08 +0100

    Save game, load game [local storage]

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ let playerCookies = document.getElementById("playerCookies");
 let playerName = document.getElementById("playerName");
 //Cookies image element
 let cookiesImage = document.getElementById("cookies");
+//Save game element
+let saveGame = document.getElementById("saveGame");
+//Load game element
+let loadGame = document.getElementById("loadGame");
 
 //Player Object
 let player = {
@@ -17,8 +21,6 @@ updateCookieCount(player.cookies);
 
 //Get Player name and display it
 player.name = 'Artur';
-
-
 playerName.innerText = player.name;
 
 //Change player  name element
@@ -35,9 +37,7 @@ let cookie = {
     count: 0
 }
 
-
 //Add +1 cookie on cookies click
-
 cookiesImage.onclick = function (e) {
     player.cookies = player.cookies + 1;
     console.log(player.cookies);
@@ -48,11 +48,29 @@ function updateCookieCount(cookieCount) {
     playerCookies.innerText = player.cookies;
 }
 
+//Save game [Local storage]
+saveGame.onclick = function (e) {
+    localStorage.setItem('player', JSON.stringify(player));
+}
+
+//Load game [Local storage]
+loadGame.onclick = function (e) {
+    let plyerData = JSON.parse(localStorage.getItem('player'));
+    console.log('Loaded player cookies: ', plyerData.cookies);
+    console.log('Loaded player name: ', plyerData.name);
+
+    playerCookies.innerText = plyerData.cookies;
+    player.cookies = plyerData.cookies;
+    playerName.innerText = plyerData.name;
+    player.name = plyerData.name;
+}
+
+
+
 
 //todo:
 /*
-add cookie on click
-SaveGame (cookie? local?)
+LoadGame (cookie doesnt work)
 Achevments
 Statistics
 */
