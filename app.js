0f437f784423ecce86d4e8a6a1532a6ebeba94a8
Artur M <artur@butio.pl>
2018-02-07 17:56:13 +0100

    Update cookie count

--- app.js
+++ app.js
@@ -1,25 +1,58 @@
 'use strict';
 
-//player Object
-var player = {
-} 
+//Defining DOM variables (elements)
+//Player cookies count element
+let playerCookies = document.getElementById("playerCookies");
+//Player  name element
+let playerName = document.getElementById("playerName");
+//Cookies image element
+let cookiesImage = document.getElementById("cookies");
+
+//Player Object
+let player = {
+    cookies: 0
+}
+
+updateCookieCount(player.cookies);
 
 //Get Player name and display it
 player.name = 'Artur';
-let playerName = document.getElementById("playerName");
+
+
 playerName.innerText = player.name;
 
-//Change player name on button click
+//Change player  name element
 let changePlayerName = document.getElementById("changePlayerName");
-changePlayerName.onclick = function(e){
+
+//Change player name on button click
+changePlayerName.onclick = function (e) {
     player.name = window.prompt('Change player name:', "Player name");
     playerName.innerText = player.name;
-    
 }
+
+//Cookie object
+let cookie = {
+    count: 0
+}
+
+
+//Add +1 cookie on cookies click
+
+cookiesImage.onclick = function (e) {
+    player.cookies = player.cookies + 1;
+    console.log(player.cookies);
+    updateCookieCount(player.cookies);
+}
+
+function updateCookieCount(cookieCount) {
+    playerCookies.innerText = player.cookies;
+}
+
 
 //todo:
 /*
+add cookie on click
 SaveGame (cookie? local?)
 Achevments
 Statistics
-*/ 
+*/
